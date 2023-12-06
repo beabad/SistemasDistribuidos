142a976ad67028880f940d54489d272f25f003d5
lasaezm <72463895+lasaezm@users.noreply.github.com>
2023-12-06 14:32:55 +0100

    Add files via upload

---
+++ src/Cliente.java
@@ -0,0 +1,173 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
+public class Cliente {
+
+	public static void main(String[] args) {
+
+		try(Socket s = new Socket("localhost",8000);
+			DataInputStream in = new DataInputStream(s.getInputStream());
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			DataInputStream teclado = new DataInputStream(System.in);)
+		{
+			System.out.println("------------------------------------------");
+			System.out.println("---- BIENVENIDO AL JUEGO DEL AHORCADO ----");
+			System.out.println("------------------------------------------");
+			boolean salir = false;
+		    String opcion;
+		    long mejorTiempo = 1000;
+		    int numPartidas = 0;
+		    
+		    while(!salir){
+		    	   System.out.println("\n--- MENÚ ---");
+		           System.out.println("1. Obtener una palabra aleatoria (1 jugador).");
+		           System.out.println("2. 2 jugadores: uno escribe la palabra y otro adivina.");
+		           System.out.println("3. 2 jugadores: los dos contra la misma palabra");
+		           System.out.println("4. Salir");
+		           
+		           System.out.print("Escribe una opción: ");
+		           opcion = teclado.readLine();
+		           out.writeBytes(opcion+"\n");//mando la opcion que he elegido
+		           out.flush();
+		           String linea, letra;
+		           long tiempo, tiempoInicial;
+		           //if(Integer.valueOf(opcion)==1) {
+		           switch(opcion.trim()){
+		               case "1":
+		                    System.out.println("\n¡El juego ha comenzado! \n");
+		                    tiempoInicial = System.currentTimeMillis();
+		       				System.out.println(in.readLine());//1
+			       			linea = in.readLine();//2
+			       			System.out.println(linea);
+			       			while(linea!=null && !linea.equals(" ")) {
+			       				letra = teclado.readLine().toLowerCase(); //Transformamos la letra a minuscula.
+			       				out.writeBytes(letra+"\n");//3
+			       				out.flush();
+			       				System.out.println(in.readLine());//4
+			       				linea = in.readLine();
+			       				System.out.println(linea);
+			       			}
+			       			numPartidas++;
+			       			tiempo = mostrarFinPartida(tiempoInicial);
+			       			if(mejorTiempo>tiempo) {
+			       				mejorTiempo=tiempo;
+			       			}
+			       			mejorTiempo(mejorTiempo, numPartidas);
+		                    break;
+		               
+		               case "2":
+		            	   try(Socket s1 = new Socket("localhost", 8000);
+		            		   DataInputStream in1 = new DataInputStream(s1.getInputStream());
+		            		   DataOutputStream out1 = new DataOutputStream(s1.getOutputStream());
+		            			DataInputStream teclado1 = new DataInputStream(System.in);
+		            		){
+		            		   System.out.println("\n¡El juego ha comenzado! \n");
+		            		   tiempoInicial = System.currentTimeMillis();
+			            	   System.out.println(in.readLine());
+		            		   String palabraMandar = teclado.readLine();
+		            		   out.writeBytes(palabraMandar+"\n");
+		            		   out.flush();
+		            		   System.out.println(in1.readLine());
+				       			linea = in1.readLine();//2
+				       			System.out.println(linea);
+				       			while(linea!=null && !linea.equals(" ")) {
+				       				letra = teclado1.readLine().toLowerCase(); //Transformamos la letra a minuscula.
+				       				out1.writeBytes(letra+"\n");//3
+				       				out1.flush();
+				       				System.out.println(in1.readLine());//4
+				       				linea = in1.readLine();
+				       				System.out.println(linea);
+				       			}
+				       			tiempo = mostrarFinPartida(tiempoInicial);
+		            	   }catch (IOException e){
+	           					e.printStackTrace();
+	           				}
+		            	   
+		                    break;
+		               case "3":
+		            	   try( Socket s1 = new Socket("localhost", 7000);
+			            	   DataInputStream in1 = new DataInputStream(s1.getInputStream());
+			            	   DataOutputStream out1 = new DataOutputStream(s1.getOutputStream());
+			            	   DataInputStream teclado1 = new DataInputStream(System.in);
+			            	){
+		            		   	System.out.println("\n¡El juego ha comenzado! \n");
+			                    
+		            		   	//Jugador 1
+		            		   	tiempoInicial = System.currentTimeMillis();
+		            		   	System.out.println(in.readLine());
+		            		   	linea = in.readLine();//2
+				       			System.out.println(linea);
+				       			while(linea!=null && !linea.equals(" ")) {
+				       				letra = teclado.readLine().toLowerCase(); //Transformamos la letra a minuscula.
+				       				out.writeBytes(letra+"\n");//3
+				       				out.flush();
+				       				System.out.println(in.readLine());//4
+				       				linea = in.readLine();
+				       				System.out.println(linea);
+				       			}
+				       			tiempo = mostrarFinPartida(tiempoInicial);
+				       			
+				       			//Jugador 2
+				       			long tiempoInicial1 = System.currentTimeMillis();
+				       			System.out.println(in.readLine());//1
+				       			String linea1 = in.readLine();//2
+				       			System.out.println(linea1);
+				       			while(linea1!=null && !linea1.equals(" ")) {
+				       				String letra1 = teclado1.readLine().toLowerCase(); //Transformamos la letra a minuscula.
+				       				out1.writeBytes(letra1+"\n");//3
+				       				out1.flush();
+				       				System.out.println(in1.readLine());//4
+				       				linea1 = in1.readLine();
+				       				System.out.println(linea1);
+				       			}
+				       			long tiempo1 = mostrarFinPartida(tiempoInicial1);
+				       			
+				       			out.writeLong(tiempo);	//manda tiempo el jug1
+		            		   	out.flush();
+		            		   	out1.writeLong(tiempo1); //manda tiempo el jug2
+		            		   	out1.flush();
+		            		   	
+		            		   	System.out.println(in.readLine());		//jug1 recibe si gana o pierde
+		            		   	System.out.println(in1.readLine());		//jug2 recibe si gana o pierde
+		            		   	
+			            	   }catch (IOException e){
+		           					e.printStackTrace();
+		           			   }
+		            	   
+		                    break;
+		               case "4":	      
+		            	   System.out.println("\nHasta pronto!!");
+		                   salir=true;
+		                   break;
+		               default:
+		                   System.out.println("Opción no válida. Elige otra.");
+		           }
+		    }
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	private static long mostrarFinPartida(long tiempoInicial) {
+		long tiempo = (System.currentTimeMillis() - tiempoInicial)/1000;
+		System.out.println("Has tardado " + tiempo + " segundos.\n");
+		System.out.println("------------------------------------");
+		System.out.println("---- Ha terminado la partida :( ----");
+		System.out.println("------------------------------------");
+		return tiempo;
+	}
+	
+	private static void mejorTiempo(long mejorTiempo, int numPartidas) {
+		if(numPartidas==1) {
+			System.out.println("Llevas " + numPartidas + " partida jugada.");
+		}
+		else {
+			System.out.println("Llevas " + numPartidas + " partidas jugadas. Tu record es " + mejorTiempo + " segundos.");
+		}
+		System.out.println("Puedes volver a jugar si lo deseas.");
+	}
+	
+}
---
+++ src/PeticionAhorcado.java
@@ -0,0 +1,328 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class PeticionAhorcado extends Thread{
+	
+	private Socket s;
+	//private static List<String> usuarios = new ArrayList();
+	
+	public PeticionAhorcado(Socket s) {
+		super();
+		this.s=s;
+		//String datos=s.getLocalAddress().toString()+" "+s.getLocalPort();
+		//usuarios.add(datos);		
+	}
+	
+	public void run() {
+		try(DataInputStream in = new DataInputStream(s.getInputStream());
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			BufferedReader br= new BufferedReader(new InputStreamReader(new FileInputStream("./src/palabras.txt")));
+			DataInputStream teclado = new DataInputStream(System.in);)
+		{
+			boolean salir = false;
+		    String opcion;
+		    int intentos = 5;
+		    List<String> palabras = crearLista(br);
+		    String palabra, palabraOculta;
+		    int numLetras, numEspacios;
+		    
+		    while(!salir){
+		           opcion = in.readLine();
+		           //if(Integer.valueOf(opcion)==1) {
+		           switch(opcion.trim()){
+		               case "1":
+			            palabra = obtenerPalabra(palabras);   
+		       			numLetras = palabra.length();
+		       			numEspacios= numEspacios(palabra,numLetras);
+		       			palabraOculta = palabraOcultaConGuiones(palabra, numLetras);
+		       			
+		       			out.writeBytes("La palabra a adivinar tiene " + (numLetras-numEspacios) + " letras. Tienes " + intentos + " intentos. La palabra oculta es: "+palabraOculta+"\n");//1
+		       			out.flush();
+		       			jugada(palabra, palabraOculta, intentos, numLetras, out, in);
+
+		       			break;
+		            	   
+		               case "2":
+		            	   try(ServerSocket ss =  new ServerSocket(8000);){
+		            		   while(true) {
+		            			   try(Socket s1 = ss.accept();
+		            					DataInputStream in1 = new DataInputStream(s1.getInputStream());
+		            					DataOutputStream out1 = new DataOutputStream(s1.getOutputStream());
+		            				){
+		   		            	    	out.writeBytes("Introduce una letra o palabra: \n");
+		     		       				out.flush();
+		     		       				palabra = in.readLine();
+		     		       				System.out.println(palabra);
+		     		       				numLetras = palabra.length();
+		     		       				numEspacios= numEspacios(palabra,numLetras);
+		     		       				palabraOculta = palabraOcultaConGuiones(palabra, numLetras);
+		       		       				out1.writeBytes("La palabra a adivinar tiene " + (numLetras-numEspacios) + " letras. Tienes " + intentos + " intentos. La palabra oculta es: "+palabraOculta+"\n");//1
+		       		       				out1.flush();
+		       		       				jugada(palabra, palabraOculta, intentos, numLetras, out1, in1);
+				            	   }
+		            		   }
+		            	   	}catch (IOException e){
+	           					e.printStackTrace();
+	           				}	
+		            		break; 
+		               
+		               case "3":
+		            	   try(ServerSocket ss =  new ServerSocket(7000);){
+		            		   while(true) {
+		            			   try(Socket s1 = ss.accept();
+		            				   DataInputStream in1 = new DataInputStream(s1.getInputStream());
+		            				   DataOutputStream out1 = new DataOutputStream(s1.getOutputStream());
+		            				){
+		   		            	    	palabra = obtenerPalabra(palabras);   
+		     		       				numLetras = palabra.length();
+		     		       				numEspacios= numEspacios(palabra,numLetras);
+		     		       				palabraOculta = palabraOcultaConGuiones(palabra, numLetras);
+		     		       				out.writeBytes("La palabra a adivinar tiene " + (numLetras-numEspacios) + " letras. Tienes " + intentos + " intentos. La palabra oculta es: "+palabraOculta+"\n");//1
+		     		       				out.flush();
+		     		       				out1.writeBytes("La palabra a adivinar tiene " + (numLetras-numEspacios) + " letras. Tienes " + intentos + " intentos. La palabra oculta es: "+palabraOculta+"\n");//1
+		       		       				out1.flush();
+		       		       				jugada(palabra, palabraOculta, intentos, numLetras, out, in);
+		       		       				jugada(palabra, palabraOculta, intentos, numLetras, out1, in1);
+		       		       				
+		       		       				
+		       		       				long tiempo, tiempo1;
+		       		       				tiempo = in.readLong();	        //recibe tiempo el jug1
+		       		       				tiempo1 = in1.readLong();		//recibe tiempo el jug2
+		       		       				if(tiempo < tiempo1) {          //manda o que gana o que pierde a cada jugador
+		       		       					out.writeBytes("Has ganado!! Tu adversario ha tardado " +  tiempo1 + " segundos. \n");
+		       		       					out.flush();
+		       		       					out1.writeBytes("Has perdido :( Tu adversario ha tardado " +  tiempo + " segundos. \n");
+		       		       					out1.flush();
+		       		       				}
+		       		       				else {
+			       		       				out.writeBytes("Has perdido :( Tu adversario ha tardado " +  tiempo1 + " segundos.  \n");
+		       		       					out.flush();
+		       		       					out1.writeBytes("Has ganado!! Tu adversario ha tardado " +  tiempo1 + " segundos.  \n");
+		       		       					out1.flush();
+		       		       				}
+				            	   }
+		            		   }
+		            	   	}catch (IOException e){
+	           					e.printStackTrace();
+	           				}	
+		            		break;
+		            	   
+		              
+		               case "4":
+		            	   salir=true;
+		            	   break;
+		           }
+		    }
+		} catch (IOException e){
+			e.printStackTrace();
+		} 
+	}
+	
+	private String obtenerPalabra(List<String> palabras) {
+			Random random = new Random();
+	    	random = new Random();
+			int indice = random.nextInt(palabras.size());
+			String palabra = palabras.get(indice).toLowerCase();//Obtenemos la palabra random en minisculas.
+			return palabra;
+	}
+	
+	
+	private void jugada(String palabra, String palabraOculta, int intentos, int numLetras, DataOutputStream out, DataInputStream in) {
+		try {
+			while (palabraOculta.contains("-") && intentos>0) {
+				out.writeBytes("Introduce una letra o la palabra:\n");//2
+				out.flush();
+				String letra = in.readLine();//3
+				if(igualTamaño(palabra, letra)) {
+					if(palabra.equals(letra)) {
+						palabraOculta=modificarOcultaEntera(palabraOculta,palabra,numLetras);
+						out.writeBytes(mensajeGanarPalabra(letra, palabra));
+						out.flush();
+					}
+					else {
+						intentos--;
+						if(intentos==0) {//Si gastamos los intentos el juego termina
+							out.writeBytes(mensajePerderPalabra(letra, palabra, intentos));
+							out.flush();
+						}else {	//aun quedan intentos, el juego continua
+							out.writeBytes(mensajePalabraIncorrecta(letra, palabraOculta, intentos));
+							out.flush();
+						}
+					}
+				}
+				
+				else if(unaLetra(letra)) {//hay que introducir solo una letra
+					
+					if(palabra.contains(letra)) {
+						if(palabraOculta.contains(letra)) {//Vemos si la letra está en la palabra.
+							intentos--;
+							if(intentos==0) {//Si gastamos los intentos el juego termina
+								out.writeBytes(mensajePerderLetra(letra, palabra, intentos));
+								out.flush();
+							}else {	//aun quedan intentos, el juego continua
+								out.writeBytes(mensajeLetraYaEsta(letra, palabraOculta, intentos));
+								out.flush();
+							}
+						}
+						else {//sino la añade
+							palabraOculta= modificarOculta(palabraOculta,palabra,letra,numLetras);
+							if(palabraOculta.equals(palabra)) {	//si ya has acertado la palabra
+								out.writeBytes(mensajeGanarLetra(letra, palabraOculta));
+								out.flush();
+							}
+							else {	//si faltan letras por adivinar
+								out.writeBytes(mensajeLetraCorrecta(letra, palabraOculta, intentos));
+								out.flush();
+							}
+						}		
+					}
+					else {
+						intentos--;
+						if(intentos==0) {//Si gastamos los intentos el juego termina
+							out.writeBytes(mensajePerderLetra(letra, palabra, intentos));
+							out.flush();
+						}else {	//aun quedan intentos, el juego continua
+							out.writeBytes(mensajeLetraIncorrecta(letra, palabraOculta, intentos));
+							out.flush();
+						}
+					}
+				}
+				else {
+					out.writeBytes("ERROR: Introduce otra letra.\n");
+					out.flush();
+				}
+			}
+			out.writeBytes(" \n");
+			out.flush();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	
+	private String mensajeLetraYaEsta(String letra, String palabraOculta, Integer intentos) {
+		return("La letra " + letra + " ya está. Tienes " + intentos + " intentos. La palabra oculta es: "+palabraOculta + "\n");
+	}
+	
+	private String mensajeGanarPalabra(String letra, String palabraOculta) {
+		return("La palabra " + letra + " es correcta.	¡¡HAS GANADO!!\n");
+	}
+	
+	private String mensajeGanarLetra(String letra, String palabraOculta) {
+		return("La letra " + letra + " es correcta. La palabra oculta es: "+palabraOculta + ".	¡¡HAS GANADO!!\n");
+	}
+	
+	private String mensajePerderPalabra(String letra, String palabra, Integer intentos) {
+		return("La palabra " + letra + " es incorrecta. ¡¡Lo siento. Has perdido!! La palabra es: " + palabra + "\n");
+	}
+	
+	private String mensajePerderLetra(String letra, String palabra, Integer intentos) {
+		return("La letra " + letra + " es incorrecta. ¡¡Lo siento. Has perdido!! La palabra es: " + palabra + "\n");
+	}
+	
+	private String mensajeLetraCorrecta(String letra, String palabraOculta, Integer intentos) {
+		return("La letra " + letra + " es correcta. Tienes " + intentos + " intentos. La palabra oculta es: "+palabraOculta + "\n");
+	}
+	
+	private String mensajeLetraIncorrecta(String letra, String palabraOculta, Integer intentos) {
+		//intentos--;
+		return("La letra " + letra + " es incorrecta. Tienes " + intentos + " intentos. La palabra oculta es: "+palabraOculta + "\n");
+	}
+	
+	private String mensajePalabraIncorrecta(String palabraIncorrecta, String palabraOculta, Integer intentos) {
+		//intentos--;
+		return("La palabra " + palabraIncorrecta + " es incorrecta. Tienes " + intentos + " intentos. La palabra oculta es: "+palabraOculta + "\n");
+	}
+	
+	
+	private boolean igualTamaño(String palabra, String palabraOculta) {
+		if(palabra.length() == palabraOculta.length()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	private boolean unaLetra(String letra) {
+		if(letra.length() == 1 && Character.isLetter(letra.charAt(0))) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	private String palabraOcultaConGuiones(String palabra, int numLetras) {
+		String palabraOculta="";
+		for(int i=0; i<numLetras; i++) {
+				if(palabra.charAt(i) == (" ").charAt(0)){
+					palabraOculta = palabraOculta + " ";
+				}
+				else {
+					palabraOculta = palabraOculta + "-";
+				}
+		}
+		return palabraOculta;
+	}
+	
+	private Integer numEspacios(String palabra, int numLetras) {
+		int numEspacios=0;
+		for(int i=0; i<numLetras; i++) {
+			if(palabra.charAt(i) == (" ").charAt(0)){
+				numEspacios++;
+			}
+		}
+		return numEspacios;
+	}
+	
+	private List<String> crearLista (BufferedReader br){
+		
+		List<String> palabras = new ArrayList<String>(); //creamos una lista con las palabras del fichero palabra.txt
+		try {
+			String linea = br.readLine();
+			while (linea!=null) {
+				palabras.add(linea);
+				linea = br.readLine();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return palabras;
+	}
+	
+	private String modificarOcultaEntera(String palabraOculta, String palabra, int numLetras) {
+		StringBuilder sb1 = new StringBuilder(palabraOculta);
+			for(int i=0; i<numLetras; i++) {
+				for(int j=0; j<numLetras; j++) {
+					if(palabra.charAt(i) == palabra.charAt(j)){
+						sb1.setCharAt(i, palabra.charAt(j));
+					}
+				}
+			}
+		palabraOculta = sb1.toString();
+		return palabraOculta;
+	}
+	
+	private String modificarOculta(String palabraOculta, String palabra, String letra, int numLetras) {
+		StringBuilder sb = new StringBuilder(palabraOculta);
+			for(int i=0; i<numLetras; i++) {
+				if(palabra.charAt(i) == letra.charAt(0)){
+					sb.setCharAt(i, letra.charAt(0));
+				}
+			}
+		palabraOculta = sb.toString();
+		return palabraOculta;
+	}
+
+}
